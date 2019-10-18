d3ccfde50722d824936876059085211f3005952f
lukaszp54 <lukaszpindel@gmail.com>
2019-10-18 10:20:42 +0200

    lab

---
+++ lab03/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab03
+{
+    class Program
+    {
+        static double ObliczSrednia(string[] args)
+        {
+            string nazwa = string.Empty;
+            Console.WriteLine("Podaj nazwe: ");
+            nazwa = Console.ReadLine();
+            string tekst = String.Empty;
+            double suma = 0;
+            int licznik = -1;
+
+
+                    while (tekst != "0")
+            {
+
+                tekst = Console.ReadLine();
+                double ocena;
+                bool czyPrzekonwertowano = double.TryParse(tekst, out ocena);
+
+               
+
+            
+                if (czyPrzekonwertowano)
+                {
+
+                    if (ocena >= 0 && ocena <= 5)
+                    {
+                        suma += ocena;
+                        licznik++;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("zla opcja");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("podano niepoprawna liczbe");
+                }
+                    
+            }
+
+            double wynik = suma / licznik;
+
+            Console.WriteLine("srednia " + nazwa + "to: " + wynik);
+            return wynik;
+
+
+          
+
+        }
+         static void Main(string[] args)
+        {
+
+            string[] przedmioty = { "P1", "BOIO", "PTC" };
+            double[] srednie = new double[przedmioty.Length];
+
+            for (int i = 0;i < przedmioty.Length; i++)
+            {
+                srednie[i] = ObliczSrednia(przedmioty[i]);
+
+            }
+            double sredniaCalkowita = 0;
+            foreach (double sredniaCzastkowa in srednie)
+            {
+                sredniaCalkowita += sredniaCzastkowa;
+            }
+            Console.WriteLine("Srednia calkowita: " + sredniaCalkowita / przedmioty.Length);
+                }
+    }
+
+
+
+
+}
---
+++ lab03/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("lab03")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab03")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("248fdcb4-27ae-4f10-a0da-d4de3f2586ca")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
