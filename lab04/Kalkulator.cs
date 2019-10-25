cad0dcad836fffc6068ce15279cc80f5ca4378b4
lukaszp54 <lukaszpindel@gmail.com>
2019-10-25 10:34:50 +0200

    lab

---
+++ lab04/Kalkulator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab04
+{
+    public class Kalkulator
+    {
+       public static string WczytajLiczbe(string licznik)
+        {
+            Console.WriteLine("Podaj liczbę " + licznik + ":");
+            string tekst = Console.ReadLine();
+            return tekst;
+        }
+
+       public static int Dodaj(int tekst1, int tekst2)
+        {
+            return tekst1 + tekst2;
+        }
+
+       public static int Dodaj(string tekst1, string tekst2)
+        {
+            int liczba1 = Convert.ToInt32(tekst1);
+            int liczba2 = Convert.ToInt32(tekst2);
+            return Dodaj(liczba1, liczba2);
+
+        }
+
+        public static int Odejmij(int tekst1, int tekst2)
+        {
+            return tekst1 - tekst2;
+        }
+
+        /// <summary>
+        /// funkcja dzielaca 2 liczby (jesli podzielono przez 0; zwraca  0)
+        /// </summary>
+        /// <param name="tekst1">dzielna</param>
+        /// <param name="tekst2">dzielnik</param>
+        /// <param name="powodzenie">czy sie udalo dzielenie</param>
+        /// <returns></returns>
+
+        public static double Podziel(int tekst1, int tekst2, out bool powodzenie)
+        {
+            if (tekst2 == 0)
+            {
+                powodzenie = false;
+                return 0;
+            }
+            powodzenie = true;
+            return (double)tekst1 / tekst2;
+        }
+
+        public static double PodzielZWyjatkiem(int tekst1, int tekst2)
+        {
+            if (tekst2 == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return (double)tekst1 / tekst2;
+
+
+        }
+
+        /*  static int Dodaj(int liczba1, int liczba2, int liczba3)
+          {
+
+             return Dodaj(Dodaj(liczba1, liczba2), liczba3);
+          }*/
+         public static int Pomnoz(int tekst1, int tekst2)
+        {
+            return tekst1 * tekst2;
+        }
+    }
+}
---
+++ lab04/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab04
+{
+
+    class Program
+    {
+
+     
+
+        static void Main(string[] args)
+        {
+
+            string tekst1 = Kalkulator.WczytajLiczbe("1");
+            string tekst2 = Kalkulator.WczytajLiczbe("2");
+            Console.WriteLine("Podaj operacje: ");
+            string operacja = Console.ReadLine();
+
+            int liczba1 = Convert.ToInt32(tekst1);
+            int liczba2 = Convert.ToInt32(tekst2);
+
+            switch (operacja)
+            {
+                case "+":
+                    Console.WriteLine(Kalkulator.Dodaj(tekst1, tekst2));
+                    break;
+                case "-":
+                    Console.WriteLine(Kalkulator.Odejmij(liczba1, liczba2));
+                    break;
+                case "*":
+                    Console.WriteLine(Kalkulator.Pomnoz(liczba1, liczba2));
+                    break;
+                case "/":
+                    Console.WriteLine(Kalkulator.PodzielZWyjatkiem(liczba1, liczba2));
+                    break;
+             
+                    /* double wynik = Podziel(liczba1, liczba2, out rezultat);
+
+                         if (rezultat)
+                         {
+                             Console.WriteLine(wynik);
+                         }
+                         else
+                         {
+                             Console.WriteLine("podzielono przez zero");
+                         }
+
+                    try
+                    {
+                        Console.WriteLine(PodzielZWyjatkiem(liczba1, liczba2));
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Podzielono przez zero");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Zla operacja");
+                    break;*/
+
+            }
+        
+        }
+    }
+    
+}
